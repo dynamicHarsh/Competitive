9180c3b4faa9097d7b1626a2c80d3736444378de
Harsh Sonkar <2140154@sliet.ac.in>
2025-03-11 23:23:08 +0530

    Auto-commit 11-03-2025

---
+++ A_Draw_a_Square.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+const ll mod = 1e9 + 7;
+
+#define mp make_pair
+#define pb push_back
+#define endl "\n"
+
+void solve() {
+    int l,r,u,d;
+    cin>>l>>r>>d>>u;
+
+    int side=l*l+r*r;
+    if(u*u+r*r==side && d*d+r*r==side && l*l+d*d==side) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ B_The_Third_Side.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+const ll mod = 1e9 + 7;
+
+#define mp make_pair
+#define pb push_back
+#define endl "\n"
+
+void solve() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    int ans=a[0];
+    for(int i=1;i<n;i++){
+        ans=ans+a[i]-1;
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t=1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
